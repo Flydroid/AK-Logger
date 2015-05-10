34bc23c38378e98e5ad83a521eb45fcecb46620c
Flydroid <jograesser@gmail.com>
2015-05-11 00:16:15 +0200

    canbus logic update 2

--- canbus.h
+++ canbus.h
@@ -15,7 +15,7 @@ public:
 	canbus();
 	~canbus();
 	void write(CAN_message_t msg);
-	void read(CAN_message_t msg);
+	int read(CAN_message_t msg);
 	FlexCAN can;
 	CAN_message_t broadcast; //Startbefehl für die Messmodule
 	CAN_message_t errorlog;
